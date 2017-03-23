0761c20e72c3cd51ac48b8b64e6deb5a7375e444
PAPI\Ekpenyong <king@house603.com>
2017-03-23 14:00:10 +0700

    new addition

---
+++ EC_SOLUTION/EC_SOLUTION/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EC_SOLUTION.MvcApplication" Language="C#" %>
---
+++ EC_SOLUTION/EC_SOLUTION/Models/EC_DB/MMS.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EC_SOLUTION.Models.EC_DB
+{
+    public class MMS
+    {
+    }
+}
