f0b970a20277d420bdbd689863f950ad15093309
Joseph <delegt00459@fpt.edu.vn>
2017-04-01 04:07:13 +0700

    Upload Functionality
    Upload

---
+++ EC_SOLUTION/EC_SOLUTION/Controllers/UploadController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EC_SOLUTION.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+        public ActionResult Index(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(int id, HttpPostedFileBase file, Upload upload)
+        {
+            if (ModelState.IsValid)
+            {
+                using (ECDB_EWDFINALEntities db = new ECDB_EWDFINALEntities())
+                {
+                    upload.ClaimId = id;
+                    upload.FileName = "/ClamDoc/" + file.FileName.Split('\\')[file.FileName.Split('\\').Length - 1];
+                    file.SaveAs(Server.MapPath("~/ClamDoc/") + file.FileName.Split('\\')[file.FileName.Split('\\').Length - 1]);
+                    db.Uploads.Add(upload);
+                    db.SaveChanges();
+                    return RedirectToAction("Index", "EC_CLAIMS");
+                }
+            }
+            return View();
+        }
+
+        // GET: Upload/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Upload/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
